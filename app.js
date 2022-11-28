50ade28a3ae9e0543bd7ad6fd58ea535f2ac05df
A_Kub_work <photokub@yandex.ru>
2022-11-28 21:41:20 +0300

    Feat: создание контроллера users

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const {PORT = 3000} = process.env
+//const {PORT = 3000} = process.env
+const { PORT = 3000, BASE_PATH } = process.env;
 const {Schema} = require("mongoose");
 const User = require("./models/users");
 const Card = require("./models/cards")
-const router = require('./routers/users')
+const users = require('./routes/users')
 
 
 
-app.use('/', router);
+app.use('/users', require('./routes/users'));
 
 
 
 mongoose.connect('mongodb://localhost:27017/mestodb', () => {
     console.log('Подключение базы mestodb');
-    app.listen(PORT, () =>
-        console.log(`App listening on port ${PORT}`)
+    app.listen(PORT, () => {
+            console.log(`App listening on port ${PORT}`),
+            console.log(`Ссылка на сервер ${BASE_PATH}`);
+        }
     )
 });
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,11 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const router = require('express').Router();
 const User = require("../models/users");
 
-router.post('/users', (req, res) => {
+module.exports.createUser = (req, res) => {
     const {name, about} = req.body;
     User.create({name, about})
         .then(user => res.send({data: user}))
         .catch(err => res.send(console.log(`Произошла ошибка: ${err}. Текст ошибки: ${err.message}`)))
-})
+}
 
-module.exports = router;
+
+
