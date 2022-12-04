b4f9ec9dd32f1f5d6ec3e2423fd32094de70359c
Photokub <photokub@yandex.ru>
2022-12-04 14:41:53 +0300

    Fix: обработка несуществующего пути

--- app.js
+++ app.js
@@ -26,11 +26,13 @@ app.use((req, res, next) => {
   };
   next();
 });
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
 app.use("*", (req, res) => {
   res.status(404).send({message: "404 Старница не найдена"})
 });
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
 
 
 
