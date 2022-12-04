760958657e9de612f32a972f59853b2d188a3fa1
Photokub <photokub@yandex.ru>
2022-12-04 20:11:38 +0300

    Fix: обновление данных пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -87,7 +87,7 @@ const patchUserText = async (req, res) => {
     } else if (!body.about) {
       return res.status(400).send({message: "Поле about должно быть заполнено"})
     }
-    return res.status(200).send(updatedUser)
+    return res.status(200).send(updatedUser(req, res))
   } catch (err) {
     console.error(err)
     return res.status(400).send({message: "Произошла ошибка"})
