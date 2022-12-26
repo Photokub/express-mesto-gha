31cbb83debf1a863db84dfe799632ab39f8c902d
Photokub <photokub@yandex.ru>
2022-12-26 18:34:48 +0300

    Fix: контроллер CreateUser

--- models/users.js
+++ models/users.js
@@ -7,21 +7,21 @@ validator.isEmail('foo@bar.com');
 const userSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    required: false,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
