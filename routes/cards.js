a6c28a9b4a6a78757cbdb9b341f1a9c06449c58e
A_Kub_work <photokub@yandex.ru>
2022-12-28 18:30:33 +0300

    Feat: валидация приходящих данных celebrate в cards

--- routes/cards.js
+++ routes/cards.js
@@ -7,9 +7,40 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getCards);
+
 router.post('/', createCard);
-router.delete('/:_id', deleteCard);
-router.put('/:_id/likes', putLike);
-router.delete('/:_id/likes', deleteLike);
+
+//router.delete('/:_id', deleteCard);
+router.delete('/:_id', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.object().required(),
+    owner: Joi.string().min(2),
+    likes: Joi.array().default([]),
+    createdAt: Joi.date().default(Date.now),
+  }).unknown(true),
+}), deleteCard);
+
+//router.put('/:_id/likes', putLike);
+router.put('/:_id/likes', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.object().required(),
+    owner: Joi.string().min(2),
+    likes: Joi.array().default([]),
+    createdAt: Joi.date().default(Date.now),
+  }).unknown(true),
+}), putLike);
+
+//router.delete('/:_id/likes', deleteLike);
+router.delete('/:_id/likes', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.object().required(),
+    owner: Joi.string().min(2),
+    likes: Joi.array().default([]),
+    createdAt: Joi.date().default(Date.now),
+  }).unknown(true),
+}), deleteLike);
 
 module.exports = router;
